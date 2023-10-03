24210059389b7ebd28df7dde78f4e025267fd023
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-10-03 17:46:22 +0200

    Models Update

--- IMDB_Clone/src/movie/models.py
+++ IMDB_Clone/src/movie/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Movie()
+CATEGORY_CHOICES = (
+    ('A', 'ACTION'),
+    ('D', 'DRAMA')
+    ('C', 'COMEDY')
+    ('R', 'ROMANCE')
+)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='movies')
