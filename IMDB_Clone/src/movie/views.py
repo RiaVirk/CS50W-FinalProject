e527d4a60183ec2580752594f0b1dbe13be6ad42
Maria Virk <121379426+RiaVirk@users.noreply.github.com>
2023-10-03 21:03:47 +0200

    Views and URLs Updated

--- IMDB_Clone/src/movie/views.py
+++ IMDB_Clone/src/movie/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Movie
+
+
+class MovieList(ListView):
+    model = ModelName
+    # template_name = ""
+
+
+class movieDetail(DetailView):
+    model = Movie
